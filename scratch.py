6d956bc23a870f796283c2f467157f21657a9c5e
amichne <austinmichne@gmail.com>
2018-10-06 04:05:13 -0700

    Project structure added

--- scratch.py
+++
@@ -1,6 +0,0 @@
-from db_util import DatabaseHandle
-
-x = DatabaseHandle('hiic', 'greenes2018', 'derived', 'db01.healthcreek.org')
-x.cursor.execute('select * from derived.patients_as_index')
-x.cursor.fetchall()
-print(x.cursor.rowcount)
