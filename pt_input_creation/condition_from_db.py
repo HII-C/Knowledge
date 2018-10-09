5ef3b985a4a736bb5263d686e38aa87331441a8e
kaylatapia <ktapia2@asu.edu>
2018-10-09 15:16:29 -0700

    initial commit for random ICD9 selection class. Working on testing

---
+++ pt_input_creation/condition_from_db.py
@@ -0,0 +1,84 @@
+from numpy.random import randint, shuffle
+from typing import List, Dict, Tuple
+from collections import defaultdict
+from util.db_util import DatabaseHandle
+from util.concept_util import ConceptType
+#import pymysql.connections as connections
+#import pymysql as sql
+
+#POP_N = 651047 #651047 in the DIAGNOSES_ICD table
+POP_N = 20
+
+class ConditionPopulation:
+    cond_db: DatabaseHandle = None
+    cond_list: List[int]
+    #conn: connections.Connection = None
+
+    def __init__(self, db_params):
+        self.cond_db = DatabaseHandle(**db_params)
+
+
+    def get_rand(self, tbl, n=1000) -> List[List[int]]:
+        if n > POP_N:
+            raise ValueError(f'Maximum patient count of {POP_N}')
+        tmp_ = list(range(POP_N))
+        print(tmp_)
+        shuffle(tmp_)
+        print(tmp_)
+        tmp_ = tmp_[0:n]
+        exec_str = f'''
+                        SELECT SUBJECT_ID 
+                        FROM {tbl}
+                        WHERE rand_id IN {tuple(tmp_)}
+                        LIMIT {n}'''
+        # try:
+        #     self.cursor.execute(exec_str)
+        # except sql.Error as e:
+        #     print("Error %d: %s" % (e.args[0], e.args[1]))
+
+        self.cond_list = [row[0] for row in self.cursor.fetchall()]
+        print(self.cond_list)
+
+        return self.cond_list
+
+
+    def get_result(self, conc: ConceptType, target_concept: Tuple[str], n=1000) -> Dict[int, bool]:
+        ret_dict = dict()
+        neg_count = 0
+        pos_count = 0
+
+        for subj_id in self.cond_list:
+            print(subj_id)
+            neg_count += 1
+            exec_str = f'''
+                            SELECT {conc.get_field()}
+                            FROM {conc.get_table()}
+                            WHERE subj_id = {subj_id}
+                            AND {conc.get_field()} in {target_concept}'''
+            #self.cond_list.cursor.execute(exec_str)
+            self.cursor.execute(exec_str)
+            #cons = self.cond_db.cursor.fetchall()
+            cons = self.cursor.fetchall()
+
+            print(cons)
+            if len(cons) > 0:
+                neg_count -= 1
+                if pos_count < n/2:
+                    pos_count = len(cons)
+                    ret_dict[subj_id] = {code: 1 for code in cons}
+                    print(ret_dict)
+            else:
+                ret_dict[subj_id] = {code: 0 for code in cons}
+                print(ret_dict)
+
+        return ret_dict
+
+
+    def main(self):
+        ConditionPopulation.get_rand(self, "DIAGNOSES_ICD", 10)
+
+        input_tuple: Tuple[str] = ("7100")
+        ConditionPopulation.get_result(self, ConceptType("Condition"), input_tuple, 10)
+
+cp = ConditionPopulation()
+if __name__ == "__main__": cp.main()
