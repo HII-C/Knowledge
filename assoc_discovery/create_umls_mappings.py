f875ae2987e81d2ea244aecaeae8352eb7af8b57
James Yu <jamesyu@Jamess-MacBook-Pro.local>
2018-11-12 15:47:20 -0700

    added more cases

--- assoc_discovery/create_umls_mappings.py
+++ assoc_discovery/create_umls_mappings.py
@@ -4,14 +4,10 @@ import MySQLdb as sql
 
 class MappingToUMLS:
     cond_db : DatabaseHandle = None
-    mimic_tables : List[Tuple[str,str,str],...] = [('mimic.D_LABITEMS', 'LABEL', 'LOINC'),
-                    ('mimic.D_ICD_PROCEDURES', 'SHORT_TITLE', 'ICD9'),
-                    ('mimic.D_ICD_DIAGNOSES', 'SHORT_TITLE', 'ICD9')]
 
 
-    def __init__(self, db_params, mimic_tables):
+    def __init__(self, db_params):
         self.cond_db = DatabaseHandle(**db_params)
-        self.mimic_tables = mimic_tables
 
 
     def get_code_and_str(self, db_params, tbl: str, encoding: str, str_col : str) -> List[List]:
@@ -23,7 +19,7 @@ class MappingToUMLS:
         return query
 
     #left outer join on CUI STRs
-    def create_table(self, db_params, datab: str, table: str):
+    def create_table(self, db_params, datab: str, table: str, mimic_tables : List[Tuple[str,str,str]] = [('mimic.D_LABITEMS', 'LABEL', 'LOINC'),('mimic.D_ICD_PROCEDURES', 'SHORT_TITLE', 'ICD9'),('mimic.D_ICD_DIAGNOSES', 'SHORT_TITLE', 'ICD9')]):
 
         # drop the existing table
         exec = f'''
@@ -37,14 +33,14 @@ class MappingToUMLS:
         loinc_spot = 0;
         icd9_spot = 0;
         snomed_spot = 0;
-        for x in range(0,len(self.mimic_tables)):
-            temp_str = f'''left join {self.mimic_tables[x][0]} as t{int(x+3)} on t1.STR = t{int(x+3)}.{self.mimic_tables[x][1]}
+        for x in range(0,len(mimic_tables)):
+            temp_str = f'''left join {mimic_tables[x][0]} as t{int(x+3)} on t1.STR = t{int(x+3)}.{mimic_tables[x][1]}
              '''
-            if(self.mimic_tables[x][2] == 'SNOMED' and snomed_spot == 0):
+            if(mimic_tables[x][2] == 'SNOMED' and snomed_spot == 0):
                 snomed_spot = x+3
-            if(self.mimic_tables[x][2] == 'LOINC' and loinc_spot == 0):
+            if(mimic_tables[x][2] == 'LOINC' and loinc_spot == 0):
                 loinc_spot = x+3
-            elif(self.mimic_tables[x][2] == 'ICD9' and icd9_spot == 0):
+            elif(mimic_tables[x][2] == 'ICD9' and icd9_spot == 0):
                 icd9_spot = x+3
 
             mimic_exec.append(temp_str)
@@ -54,34 +50,36 @@ class MappingToUMLS:
         if(loinc_spot != 0 and icd9_spot != 0 and snomed_spot != 0):
             select_str = f'''DISTINCT CUI, RXCUI, t{loinc_spot}.LOINC_CODE, t{icd9_spot}.ICD9_CODE, t{snomed_spot}.SNOMED_CODE'''
             temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), t{loinc_spot}.LOINC_CODE VARCHAR(200), t{icd9_spot}.ICD9_CODE VARCHAR(200), t{snomed_spot}.SNOMED_CODE VARCHAR(200))'''
-            # rename_str = f'''RENAME COLUMN t{loinc_spot}.LOINC_CODE TO LOINC_CODE
-            #                  RENAME COLUMN t{icd9_spot}.ICD9_CODE  TO ICD9_CODE
-            #                  RENAME COLUMN t{snomed_spot}.SNOMED_CODE TO SNOMED_CODE
-            #                  '''
 
         elif(loinc_spot != 0 and icd9_spot != 0):
             select_str = f'''DISTINCT CUI, RXCUI, t{loinc_spot}.LOINC_CODE, t{icd9_spot}.ICD9_CODE'''
             temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), t{loinc_spot}.LOINC_CODE VARCHAR(200), t{icd9_spot}.ICD9_CODE VARCHAR(200), SNOMED_CODE VARCHAR(200))'''
-            # rename_str = f'''RENAME COLUMN t{loinc_spot}.LOINC_CODE TO LOINC_CODE
-            #                  RENAME COLUMN t{icd9_spot}.ICD9_CODE  TO ICD9_CODE
-            #                  '''
+
+        elif(loinc_spot != 0 and snomed_spot != 0):
+            select_str = f'''DISTINCT CUI, RXCUI, t{loinc_spot}.LOINC_CODE, t{icd9_spot}.ICD9_CODE'''
+            temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), t{loinc_spot}.LOINC_CODE VARCHAR(200), ICD9_CODE VARCHAR(200), t{snomed_spot}.SNOMED_CODE VARCHAR(200))'''
+
+        elif(snomed_spot != 0 and icd9_spot != 0):
+            select_str = f'''DISTINCT CUI, RXCUI, t{snomed_spot}.SNOMED_CODE, t{icd9_spot}.ICD9_CODE'''
+            temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), LOINC_CODE VARCHAR(200), t{icd9_spot}.ICD9_CODE VARCHAR(200), t{snomed_spot}.SNOMED_CODE VARCHAR(200))'''
+
 
         elif(loinc_spot != 0):
             select_str = f'''DISTINCT CUI, RXCUI, t{icd9_spot}.ICD9_CODE'''
             temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), t{loinc_spot}.LOINC_CODE VARCHAR(200), ICD9_CODE VARCHAR(200), SNOMED_CODE VARCHAR(200))'''
-            # rename_str = f'''RENAME COLUMN t{loinc_spot}.LOINC_CODE TO LOINC_CODE
-            #                  '''
+
 
         elif(icd9_spot != 0):
             select_str = f'''DISTINCT CUI, RXCUI, t{loinc_spot}.LOINC_CODE'''
             temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), LOINC_CODE VARCHAR(200), t{icd9_spot}.ICD9_CODE VARCHAR(200), SNOMED_CODE VARCHAR(200))'''
-            # rename_str = f'''RENAME COLUMN t{icd9_spot}.ICD9_CODE  TO ICD9_CODE
-            #                  '''
+
+        elif(snomed_spot != 0):
+            select_str = f'''DISTINCT CUI, RXCUI, t{snomed_spot}.SNOMED_CODE'''
+            temp_col_names = f'''(CUI VARCHAR(200), RXCUI VARCHAR(200), LOINC_CODE VARCHAR(200), ICD9_CODE VARCHAR(200), t{snomed_spot}.SNOMED_CODE VARCHAR(200))'''
 
         else:
             select_str = f'''DISTINCT CUI, RCUI'''
             temp_col_names = '(CUI VARCHAR(200), RXCUI VARCHAR(200), LOINC_CODE VARCHAR(200), ICD9_CODE VARCHAR(200), SNOMED_CODE VARCHAR(200))'
-            # rename_str = ''
 
         # create new table
         exec = f''' 
@@ -92,6 +90,7 @@ class MappingToUMLS:
                left join rxnorm.RXNCONSO as t2 on t1.STR = t2.STR 
                {mimic_exec_str};
                '''
+        
 
         print(exec)
         self.cond_db.cursor.execute(exec)
