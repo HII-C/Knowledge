3a7dfb6cc3617c893d7e65e33134667efd80a2b9
kaylatapia <ktapia2@asu.edu>
2018-11-18 22:07:26 -0700

    creates tables labevents_UMLS and diagnoses_UMLS with temporary table ICD9TOCUI

--- assoc_discovery/create_umls_cui_mapping.py
+++ assoc_discovery/create_umls_cui_mapping.py
@@ -41,10 +41,33 @@ class CreateUMLSCUIMapping:
 
         # self.connection.commit()
 
-    #def create_diagnoses_UMLS(self):
+    def diagnoses_ICD_to_CUI(self, output_table, n=10000, output_db='derived'):
+        temp_table = "ICDTOCUI"
 
+        exec_str = f'''
+                    CREATE TABLE {output_db}.{temp_table} (CUI CHAR(8), AUI VARCHAR(9), SAB VARCHAR(40), ICD9_CODE VARCHAR(255)) 
+                    AS SELECT umls.MRCONSO.CUI as CUI, umls.MRCONSO.AUI as AUI, umls.MRCONSO.SAB as SAB, mimic.D_ICD_DIAGNOSES.ICD9_CODE as ICD9_CODE
+                    FROM umls.MRCONSO INNER JOIN
+                    mimic.D_ICD_DIAGNOSES ON umls.MRCONSO.STR = mimic.D_ICD_DIAGNOSES.SHORT_TITLE limit {n}'''
 
-    #def icd9_to_umls_cui(self):
+        output_table = "diagnoses_UMLS"
+        exec_str = f'''
+                    CREATE TABLE {output_db}.{output_table} (CUI CHAR(8), ROW_ID INT, SUBJECT_ID INT, HADM_ID INT, SEQ_NUM INT)
+                    AS SELECT derived.ICDTOCUI.CUI as CUI, mimic.DIAGNOSES_ICD.ROW_ID as ROW_ID, 
+                    mimic.DIAGNOSES_ICD.SUBJECT_ID as SUBJECT_ID, mimic.DIAGNOSES_ICD.HADM_ID as HADM_ID, mimic.DIAGNOSES_ICD.SEQ_NUM as SEQ_NUM 
+                    FROM mimic.DIAGNOSES_ICD INNER JOIN
+                    derived.ICDTOCUI on mimic.DIAGNOSES_ICD.ICD9_CODE = derived.ICDTOCUI.ICD9_CODE limit {n}'''
+
+    def create_diagnoses_UMLS(self, output_table, n=10000, output_db='derived'):
+        output_table = "diagnoses_UMLS"
+
+        exec_str =  f'''
+                    CREATE TABLE {output_db}.{output_table}(CUI CHAR(8), ROW_ID INT, SUBJECT_ID INT, HADM_ID INT, CHARTTIME DATETIME, VALUE TEXT, VALUENUM FLOAT, VALUEUOM VARCHAR(255), FLAG VARCHAR(255))
+                    AS SELECT mimic.ItemIdToCUI.CUI as CUI, mimic.LABEVENTS.ROW_ID as ROW_ID, 
+                    mimic.LABEVENTS.SUBJECT_ID as SUBJECT_ID, mimic.LABEVENTS.HADM_ID as HADM_ID, mimic.LABEVENTS.CHARTTIME as CHARTTIME, 
+                    mimic.LABEVENTS.VALUE as VALUE, mimic.LABEVENTS.VALUENUM as VALUENUM, mimic.LABEVENTS.VALUEUOM as VALUEUOM, mimic.LABEVENTS.FLAG as FLAG
+                    FROM mimic.LABEVENTS INNER JOIN
+                    mimic.ItemIdToCUI ON mimic.LABEVENTS.ITEMID = mimic.ItemIdToCUI.ITEMID limit {n}'''
 
 #     def main(self):
 #         CreateUMLSCUIMapping.get_occurences(self, output_table="labevents_UMLS", n=10, output_db="derived")
