e71d7409d37b2bc92d6aa7becf3c7d99dd51cc4a
kaylatapia <ktapia2@asu.edu>
2019-03-19 11:20:20 -0700

    still working

--- models/util/data_util.py
+++ models/util/data_util.py
@@ -11,11 +11,16 @@ class Dataset:
 
     def drop_ratio(self, ratio=.10):
         ''' Drop all columns in x for which a positive value occurs
-            in less the (N_rows * ratio) rows. TO BE IMPLEMENTED'''
+            in less then (N_rows * ratio) rows. TO BE IMPLEMENTED'''
+        # Note: 0 - row, 1 - column
+        x: pd.DataFrame = None
+
+        dp_ratio = x.shape[1] * ratio
+        x.drop(x.columns[x.apply(lambda col: col.value() > 0).sum(axis=0) <= dp_ratio], axis=1)
         pass
 
     def drop_lowest_occ(self, n=40):
-        ''' Drop columns the occur the least in all rows. 
+        ''' Drop columns that occur the least in all rows.
             For binary this is sum(x[column]). 
             Should end len(x.shape[1]) = len(x.shape[1]) - n'''
         pass
