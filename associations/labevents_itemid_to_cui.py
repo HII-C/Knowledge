ae8402eb8e0097e801876ad00298e6d2b7eae05d
jamesyu5421 <jamesyu@Jamess-MacBook-Pro.local>
2019-04-16 23:36:05 -0700

    temp tables dont have enough data so another statement is being tested

--- associations/labevents_itemid_to_cui.py
+++ associations/labevents_itemid_to_cui.py
@@ -18,29 +18,17 @@ class LabeventConversion:
                               umls_table: str = 'MRCONSO',
                               printout=False):
 
-        # creates a temp table where ITEMID is replaced by the CUI encoding
-        temp_exec_str = f''' CREATE TEMPORARY TABLE {output_db}.temp
-                        AS SELECT 
-                            SUBJECT_ID, HADM_ID, CHARTTIME, VALUE, VALUENUM, VALUEUOM, FLAG, LOINC_CODE
-                        FROM {mimic_db}.{input_table}
-                            LEFT JOIN {mimic_db}.{itemid_table}
-                                ON 
-                            {mimic_db}.{input_table}.ITEMID = {mimic_db}.{itemid_table}.ITEMID 
-                        limit {n}
-                        '''
-        if printout:
-            print(temp_exec_str)
-
-        self.know_db.cursor.execute(temp_exec_str)
-        self.know_db.connection.commit()
 
         exec_str = f''' CREATE TABLE {output_db}.{output_table}
                         AS SELECT 
                             SUBJECT_ID, HADM_ID, CUI, CHARTIME, VALUE, VALUENUM, VALUEUOM, FLAG
-                        FROM {output_db}.temp
+                        FROM {mimic_db}.{input_table}
+                            LEFT JOIN {mimic_db}.{itemid_table}
+                                ON 
+                            {mimic_db}.{input_table}.ITEMID = {mimic_db}.{itemid_table}.ITEMID
                             LEFT JOIN {umls_db}.{umls_table}
                                 ON 
-                            {output_db}.temp.LOINC_CODE = {umls_db}.{umls_table}.CODE 
+                            {output_db}.{output_db}.LOINC_CODE = {umls_db}.{umls_table}.CODE 
                         limit {n}
                     '''
         if printout:
